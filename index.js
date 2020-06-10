70701a1e1d4253c110eda246a079cf2bf4e634ce
subiabre <subiabrewd@gmail.com>
2020-06-11 00:40:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,33 +4,35 @@ const app = express();
 const showdown = require('showdown');
 const converter = new showdown.Converter();
 
+const blog = require('./src/blogObject');
+
 const postsLoader = require('./src/postsLoader');
 const loader = new postsLoader;
-const postsList = loader.loadFromFolder('./posts');
+const postsList = loader.loadFromFolder(blog.order);
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', {posts: postsList});
+    res.render('index', {blog, posts: postsList});
 });
 
 app.get('/:id', (req, res) => {
     let id = req.params.id;
     let post = loader.loadById(id);
 
-    if (!post) res.render('post', {post: {
+    if (!post) res.render('post', {blog, post: {
         metadata: {title: '404'},
         content: 'No post found'
     }});
 
     if (post) post.content = converter.makeHtml(post.content);
 
-    res.render('post', {post});
+    res.render('post', {blog, post});
 })
 
-app.listen(3000, function () {
-    let stratter = require('stratter')
-    let log = stratter('Bloggit listening on http://localhost:3000', {
+const listener = app.listen(process.env.PORT || 3000, () => {
+    let stratter = require('stratter');
+    let log = stratter(`Bloggit listening on http://localhost:${listener.address().port}`, {
         foreground: 'white',
         background: 'green'
     });
