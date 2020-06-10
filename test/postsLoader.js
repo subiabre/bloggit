8b200850679776c5f6529f5aa934946e32bc810e
subiabre <subiabrewd@gmail.com>
2020-06-10 17:42:19 +0200

    Add single post loading test

--- test/postsLoader.js
+++ test/postsLoader.js
@@ -6,12 +6,21 @@ const assert = chai.assert;
 
 describe('postsLoader', () => {
     it('should retrieve an array with the files inside default folder', async function() {
-        const posts = new PostsLoader;
-        const postsList = posts.loadFromFolder();
+        const loader = new PostsLoader;
+        const postsList = loader.loadFromFolder();
         
         assert.isArray(postsList);
         assert.isObject(postsList[0]);
         assert.exists(postsList[0].metadata);
         assert.exists(postsList[0].content);
+    });
+
+    it('should retrieve a single post by id', async function() {
+        const loader = new PostsLoader;
+        const post = loader.loadById('1');
+
+        assert.isObject(post);
+        assert.exists(post.metadata);
+        assert.exists(post.content);
     });
 });
